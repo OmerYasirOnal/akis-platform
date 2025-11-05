25427809115492626b58fb84e99806053cb871cf
OmerYasirOnal <engomeryasironal@gmail.com>
2025-11-05 20:51:53 +0300

    Fastify mimarisi için son cursor kurallarını ekle

--- backend/src/core/agents/IAgent.ts
+++
@@ -1,8 +0,0 @@
-import { AgentContract, AgentExecutionContext, AgentResult, AgentType } from '../contracts/AgentContract';
-
-export interface IAgent<TContext extends AgentExecutionContext = AgentExecutionContext> extends AgentContract<TContext> {
-  readonly type: AgentType;
-  execute(context: TContext): Promise<AgentResult>;
-}
-
-
