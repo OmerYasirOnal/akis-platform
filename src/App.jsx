97f7e41b1eb603d5d498166065c0ec9b6d742ddb
Ömer Yasir Önal <115497334+OmerYasirOnal@users.noreply.github.com>
2026-03-21 03:33:18 +0300

    feat: add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="app-title">Basit Sayaç</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
